34c5e72ce2e14849d7815d981eb9cd6f4478a8af
Eric <ericmcmillan@yahoo.com>
2020-04-27 17:38:22 -0500

    add comments

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express"); // imports express node server
 const redis = require("redis"); // imports in redis client in order to talk to the redis server
-const process = require("process"); 
+const process = require("process");  // Used to simulate a container crash.  
 
 const app = express(); // Create a new instance of an express application
 
 // Set up a connection to our redis server
 const client = redis.createClient({ // Varible client is the connection to the redis server 
   host: "redis-server", // redis-server defined in the docker-compose.yml file
-  port: 6379
+  port: 6379 // Default port that redis server uses
 });
 
 client.set("visits", 0); // Initialize `visits` to be zero on startup
@@ -15,7 +15,7 @@ client.set("visits", 0); // Initialize `visits` to be zero on startup
 // A route handler for our root route i.e. the "/" i.e. localhost:8081
 // Anytime someone comes to our root route call the callback function (req, res)
 app.get("/", (req, res) => {
-  //process.exit(0)
+  //process.exit(0) // uncomment this line to simulate a container failure so docker-compose.yml's restart: on-failure flag kicks in
   
   // Call the redis server to get the the number of 'visits'.  
   // Redis server will pass back the 'visits' as a string in the callback function (err, visits)
